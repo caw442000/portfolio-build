0e9993d41dde773498d09ac8c206e7e368ea9811
cedric-winbush <cawinbushjr@gmail.com>
2020-08-21 14:59:20 -0500

    completes slider draw

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import MobileRightMenuSlider from "@material-ui/core/drawer"
 import {
   AppBar,
   Toolbar,
   ListItem,
   IconButton,
   ListItemText,
+  ListItemIcon,
   Avatar,
   Divider,
   List,
@@ -19,21 +21,98 @@ import {
   ContactMail,
 } from "@material-ui/icons";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+import cedric from "../assets/images/cedric.png";
+
+//CSS Styles
+
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 250,
+    background: "#511",
+    height: "100%",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "tan",
+  },
+}));
+
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contacts",
+  },
+];
+
 const NavBar = () => {
+  const [state, setState] = useState({
+    right: false
+  })
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+
+  // const toggleSlider = (slider, open ) => () => {
+  //   setState({...state, [slider]: open});
+
+  // };
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const classes = useStyles();
+  const sideList = () => (
+    <Box className={classes.menuSliderContainer} component="div">
+      <Avatar className={classes.avatar} src={cedric} alt="Cedric Winbush Jr" />
+      <Divider />
+      <List>
+        {menuItems.map((lsItem, key) => (
+          <ListItem key={key} button>
+            <ListItemIcon className={classes.listItem}>
+              {lsItem.listIcon}
+            </ListItemIcon>
+            <ListItemText primary={lsItem.listText} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <>
-    <Box component="nav">
-      <AppBar position="static" style = {{ background: "#222"}}>
-        <Toolbar>
-          <IconButton>
-            <ArrowBack style = {{ color: "tomato"}}/>
-          </IconButton>
-          <Typography variant = "h5" style = {{color: "tan"}}>
-            Portfolio
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton onClick={handleDrawerToggle}>
+              <ArrowBack style={{ color: "tomato" }} />
+            </IconButton>
+            <Typography variant="h5" style={{ color: "tan" }}>
+              Portfolio
+            </Typography>
+            <MobileRightMenuSlider anchor="right" open= {mobileOpen} onClose = {handleDrawerToggle} >
+                {sideList()}
+            </MobileRightMenuSlider>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </>
   );
 };
