da5c7ab345aa449e5f2f8c411420266758f900da
cedric-winbush <cawinbushjr@gmail.com>
2020-09-28 13:58:00 -0500

    cleans Home page comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,30 +3,19 @@ import NavBar from "./NavBar";
 import HomeBody from "./HomeBody";
 import ParticleComponent from "./ParticleComponent";
 
-
 const Home = () => {
   return (
-  
     <div
       style={{
         width: "100%",
         height: "100%",
         margin: "0 auto",
-        // padding: "0",
-        // flex: '1'
       }}
     >
       <ParticleComponent />
-      {/* <div
-        style={{
-          overflow: 'hidden'
-        }}
-      > */}
-        {/* <NavBar /> */}
-        <HomeBody />
-      {/* </div> */}
+
+      <HomeBody />
     </div>
-    // </>
   );
 };
 
