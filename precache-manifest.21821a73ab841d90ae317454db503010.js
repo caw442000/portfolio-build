b300dd9dd8113d994ce5b30918dd26b9dade0038
cedric-winbush <cawinbushjr@gmail.com>
2020-10-04 15:29:41 -0500

    Updates

---
+++ precache-manifest.21821a73ab841d90ae317454db503010.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6601a158f191f0b76dc03296a33da40",
+    "url": "/portfolio-build/index.html"
+  },
+  {
+    "revision": "7d6fb9c8c77a18e62c7f",
+    "url": "/portfolio-build/static/css/main.6fd720ba.chunk.css"
+  },
+  {
+    "revision": "ca02f022c3b23162df44",
+    "url": "/portfolio-build/static/js/2.4c0d15ab.chunk.js"
+  },
+  {
+    "revision": "9100242b443132c58e8b72511848626b",
+    "url": "/portfolio-build/static/js/2.4c0d15ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6fb9c8c77a18e62c7f",
+    "url": "/portfolio-build/static/js/main.89216d20.chunk.js"
+  },
+  {
+    "revision": "8c7be53c59cb34e1fbfc",
+    "url": "/portfolio-build/static/js/runtime-main.586a285d.js"
+  },
+  {
+    "revision": "fedb3541a0599f7bb3ee9e47a3f547ea",
+    "url": "/portfolio-build/static/media/cedric-winbush-wbg.fedb3541.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio-build/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "32ae300ba6b529e143ed10463aabb293",
+    "url": "/portfolio-build/static/media/signlingo.32ae300b.png"
+  },
+  {
+    "revision": "70bcef9b34996659df40377014137df3",
+    "url": "/portfolio-build/static/media/teaching.70bcef9b.jpg"
+  },
+  {
+    "revision": "3e9af902b0b6bda21ad6e1e8eea44641",
+    "url": "/portfolio-build/static/media/washing-dishes.3e9af902.jpg"
+  },
+  {
+    "revision": "8bed36f2fe2f18234a5fbabf991bff0f",
+    "url": "/portfolio-build/static/media/workout.8bed36f2.jpg"
+  }
+]);
